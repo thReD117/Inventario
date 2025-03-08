8b72979ed53d98f6cb0eaeb3a0ec639849812127
thReD117 <hombr@LAXR3D>
2025-03-08 15:11:14 -0600

    Prueba 1

--- src/main/java/com/mycompany/inventariofinal/InventarioFinal.java
+++ src/main/java/com/mycompany/inventariofinal/InventarioFinal.java
@@ -2,5 +2,6 @@ package com.mycompany.inventariofinal;
 public class InventarioFinal {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Negromorfo");
     }
 }
