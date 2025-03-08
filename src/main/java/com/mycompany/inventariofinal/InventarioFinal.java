bdd6686ef18f103ee73d3dfbe2ff34c4a4cd8f6b
marko <marko@LAPTOP-6SUNL5RM>
2025-03-08 15:16:23 -0600

    Si jala

--- src/main/java/com/mycompany/inventariofinal/InventarioFinal.java
+++ src/main/java/com/mycompany/inventariofinal/InventarioFinal.java
@@ -3,5 +3,7 @@ public class InventarioFinal {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Negromorfo");
+        
+        System.out.println("Virgen");
     }
 }
