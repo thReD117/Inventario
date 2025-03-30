1e2fba5e44c7e12800f9e85071e8aedb6cf4a150
Marco Solís <marko@LAPTOP-6SUNL5RM>
2025-03-30 13:35:16 -0600

    Pruebas básicas de conexión y uso de base de datos

---
+++ src/main/java/Testing/Database.java
@@ -0,0 +1,96 @@
+package Testing;
+
+import Enums.CategoryEnumerable;
+import Enums.CategorySup;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Database {
+    private static final String url = "jdbc:postgresql://db.zjtaizpyifuycbatgkao.supabase.co:5432/postgres";
+    private static final String usr = "postgres";
+    private static final String pwd = "SWhN9NJaHsYrtMtO";
+    
+    public static void main(String[] args) {
+//        createNewTable();
+//        insertIntoClothing("6203A11A6403", "00001", CategorySup.SHIRTS);
+//        insertIntoClothing("6203A11ADF11", "00001", CategorySup.BLOUSES);
+//        
+        
+        selectAllFromClothing().forEach(System.out::println);
+    }
+    
+    public static void createNewTable(){
+        // SQL query
+        String query = """
+                       CREATE TABLE IF NOT EXISTS CLOTHING(
+                            GROUP_ID VARCHAR(50),
+                            SINGULAR_ID VARCHAR(30),
+                            CATEGORY VARCHAR(50)
+                       )
+                       """;
+
+        // Connect to database
+        try (Connection conn = DriverManager.getConnection(url, usr, pwd)){
+            Statement st = conn.createStatement(); // Execute query
+            st.execute(query);
+            
+            System.out.println("Tabla creada");
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void insertIntoClothing(String groupId, String singularId, CategoryEnumerable category){
+        // Different query structure when using variables
+        String query = """
+                       INSERT INTO CLOTHING(GROUP_ID, SINGULAR_ID, CATEGORY)
+                       VALUES (?,?,?)
+                       """;
+        
+        try (Connection conn = DriverManager.getConnection(url, usr, pwd)) {
+            // Prepared statement helps to replace variables in the query
+            PreparedStatement prepSt = conn.prepareStatement(query);
+            
+            // Replace variables into query
+            prepSt.setString(1, groupId);
+            prepSt.setString(2, singularId);
+            prepSt.setString(3, category.getCategory());
+            
+            prepSt.execute();
+            
+            System.out.println("Datos insertados");
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    // Por el momento devuelve un aL de string pero aquí se devolveria el objeto o se añadirían al stock u otra mierda
+    public static ArrayList<String> selectAllFromClothing(){
+        String query = "SELECT * FROM CLOTHING";        
+
+        ArrayList<String> rows = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(url, usr, pwd)) {
+            Statement st = conn.createStatement();
+            
+            // Stores all the data retrieved by the query
+            ResultSet rs = st.executeQuery(query);
+            
+            // It has to iterate through the set in order to retrieve the info
+            while(rs.next()){
+                rows.add(String.format("%s - %s | %s", 
+                        rs.getString("GROUP_ID"),
+                        rs.getString("SINGULAR_ID"),
+                        rs.getString("CATEGORY")
+                ));
+            }
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return rows;
+    }
+}
